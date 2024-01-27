b7db96a736c9a02667001f179c1b2c7933053db4
Dawson Knudtson <300078204@fvtc.edu>
2024-01-26 19:46:16 -0600

    Add project files.

---
+++ DRK.ProgDec.BL.Models/DegreeType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRK.ProgDec.BL.Models
+{
+    internal class DegreeType
+    {
+    }
+}
