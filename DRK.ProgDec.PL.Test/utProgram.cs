078e4d3c4bde6cae1317fc2882e25c5f27021431
Dawson Knudtson <300078204@fvtc.edu>
2024-02-12 17:50:51 -0600

    Added utProgram LoadTest and InsertTest

--- DRK.ProgDec.PL.Test/utProgram.cs
+++ DRK.ProgDec.PL.Test/utProgram.cs
@@ -13,5 +13,29 @@ namespace DRK.ProgDec.PL.Test
 
         }
 
+        [TestMethod]
+
+        public void InsertTest()
+        {
+            var dc = new ProgDecEntities();
+
+            // make entity
+            tblProgram entity = new tblProgram();
+
+            entity.DegreeTypeId = 2;
+            entity.Description = "basket weaving";
+            entity.Id = -99;
+            // add entity to DB
+            dc.tblPrograms.Add(entity);
+
+            // commit changes
+            int result = dc.SaveChanges();
+            Assert.AreEqual(1, result);
+
+        }
+
+
+
+
     }
 }
