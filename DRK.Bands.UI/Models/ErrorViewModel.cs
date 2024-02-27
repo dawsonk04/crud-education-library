8a8cfbbb35bd9030a57fbcedf7ced41ac4538aa1
Dawson Knudtson <300078204@fvtc.edu>
2024-02-27 15:23:24 -0600

    Added the Bands UI

---
+++ DRK.Bands.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DRK.Bands.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
