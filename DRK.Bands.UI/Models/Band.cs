3303daa78d88b45360894e948ccbbae01388c97f
Dawson Knudtson <300078204@fvtc.edu>
2024-03-17 18:41:23 -0500

    Added images and cards

--- DRK.Bands.UI/Models/Band.cs
+++ DRK.Bands.UI/Models/Band.cs
@@ -1,4 +1,7 @@
-﻿namespace DRK.Bands.UI.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DRK.Bands.UI.Models
 {
     public class Band
     {
@@ -8,6 +11,8 @@
 
         public string Genre { get; set; }
 
+        [DisplayName("Date Founded")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime DateFounded { get; set; }
 
         public string Image { get; set; }
