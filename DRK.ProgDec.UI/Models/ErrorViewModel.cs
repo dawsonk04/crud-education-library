425d952abad7122145ff0169ca7efb6121ffc79e
Dawson Knudtson <300078204@fvtc.edu>
2024-03-18 19:12:57 -0500

    Added UI Project and Extensions for sesion

---
+++ DRK.ProgDec.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DRK.ProgDec.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
