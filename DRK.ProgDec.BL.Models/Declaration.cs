a769782caa56da731d9fe7cbe663a6474c8f52ab
Dawson Knudtson <300078204@fvtc.edu>
2024-03-25 18:13:43 -0500

    added stuff to declaration BL.Model, formatting and added a few other properties

--- DRK.ProgDec.BL.Models/Declaration.cs
+++ DRK.ProgDec.BL.Models/Declaration.cs
@@ -1,11 +1,15 @@
-﻿namespace DRK.ProgDec.BL.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DRK.ProgDec.BL.Models
 {
     public class Declaration
     {
         public int ID { get; set; }
         public int StudentID { get; set; }
         public int ProgramID { get; set; }
-
+        [DisplayName("ChangeDate")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime ChangeDate { get; set; }
 
     }
